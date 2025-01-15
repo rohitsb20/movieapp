3a1d6c498896818db353e13c79c46fc0e6ee78ef
rohit <rohitsb96134@gmail.com>
2025-01-15 16:16:34 +0530

    Wrap Navbar component in Suspense for improved loading state handling and refactor for clarity

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,8 @@
-'use client'
-import Link from 'next/link'
-import React from 'react'
-import { Button } from "@/components/ui/button";
-import { useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
 
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import{ Button} from "@/components/ui/button"; // Adjust the import based on your project structure
 
 const items = [
   {
@@ -18,22 +16,27 @@ const items = [
 ];
 
 export default function Navbar() {
-    const searchParams = useSearchParams();
-    const genre = searchParams.get('genre');
-
   return (
-     <Suspense fallback={<div>Loading...</div>}>
-    <div className="bg-rose-500 flex justify-center items-center gap-x-4">
-      {items.map((item) => (
-        <Button  className={` ${genre === item.param ? 'bg-rose-500' : ''}`} key={item.title}>
-          <Link href={`/?genre=${item.param}`}>
-            {item.title}
-          </Link>
-        </Button>
-      ))}
-    </div>
-     </Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
+      <NavbarContent />
+    </Suspense>
   );
 }
 
+function NavbarContent() {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
 
+  return (
+    <div className="bg-rose-500 flex justify-center items-center gap-x-4">
+      {items.map((item) => (
+        <Button
+          className={` ${genre === item.param ? "bg-rose-500" : ""}`}
+          key={item.title}
+        >
+          <Link href={`/?genre=${item.param}`}>{item.title}</Link>
+        </Button>
+      ))}
+    </div>
+  );
+}
